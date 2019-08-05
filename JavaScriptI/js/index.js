ceece1ddec63e0e4a4e214ae1760eac95ee768c7
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-05 15:30:49 -0300

    criada validação de dados básica

--- JavaScriptI/js/index.js
+++ JavaScriptI/js/index.js
@@ -7,5 +7,12 @@ let peso = paciente.querySelector(".info-peso").textContent;
 
 let altura = paciente.querySelector(".info-altura").textContent;
 
+if((peso < 0) || (peso > 1000)){
+  console.log("peso inválido");
+}
+
+if((altura < 0) || (altura > 3.00)){
+  console.log("altura inválida");
+}
 
 paciente.querySelector(".info-imc").textContent = peso / (altura * altura);
