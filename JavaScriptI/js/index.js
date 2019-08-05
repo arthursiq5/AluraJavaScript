2fa3abcf0d8341990b81c129ca42f81458520c08
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-05 15:08:14 -0300

    capturando dados das tags HTML

--- JavaScriptI/js/index.js
+++ JavaScriptI/js/index.js
@@ -1,2 +1,11 @@
 var titulo = document.querySelector("#titulo");
 titulo.textContent = "abacate";
+
+let paciente = document.querySelector("#primeiro-paciente");
+
+let peso = paciente.querySelector(".info-peso").textContent;
+
+let altura = paciente.querySelector(".info-altura").textContent;
+
+console.log(peso);
+console.log(altura);
