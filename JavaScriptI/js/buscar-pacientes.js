81ee98cf88863e4f8c15eaa9731b0e5dc112c347
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-07 17:23:42 -0300

    modificado para mostrar mensagens de erro HTTP

--- JavaScriptI/js/buscar-pacientes.js
+++ JavaScriptI/js/buscar-pacientes.js
@@ -4,12 +4,19 @@ botaoAdicionar.addEventListener('click', function () {
 
   let xhr = new XMLHttpRequest(); // objeto de requisições em JavaScript
 
-  xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+  xhr.open("GET", "https://api-pacientes.herokuapp.com/pacie1ntes");
 
   xhr.addEventListener('load', function(){
-    let pacientes = JSON.parse(this.responseText);
+    if ( this.status == 200 ){
+      let pacientes = JSON.parse(this.responseText);
 
-    pacientes.forEach(insereNovoPaciente);
+      pacientes.forEach(insereNovoPaciente);
+    }else{
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+      let erroAjax = document.querySelector("#erro-ajax");
+      erroAjax.classList.remove('invisivel');
+    }
   });
   xhr.send();
 });
