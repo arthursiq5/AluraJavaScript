53b7246b99502dcd1b8b3ca33c95aac318c78626
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-07 14:51:39 -0300

    removido código obsoleto

--- JavaScriptI/js/remover-pacientes.js
+++ JavaScriptI/js/remover-pacientes.js
@@ -5,11 +5,3 @@ let tabela = document.querySelector("#tabela-pacientes");
 tabela.addEventListener("dblclick", function (event) {
   event.target.parentNode.remove();
 });
-
-
-// pacientes.forEach( function (paciente) {
-//
-//   paciente.addEventListener('dblclick', function () {
-//     this.remove(); // remove o objeto atrelado ao evento
-//   });
-// });
