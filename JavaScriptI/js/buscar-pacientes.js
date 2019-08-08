d5502afaf40032699449ca0560d76e244b6ab5ec
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-08 13:36:10 -0300

    corrigidos bugs corrigidos bugs, trocada URL, refatorado sistema para melhor desempenho

--- JavaScriptI/js/buscar-pacientes.js
+++ JavaScriptI/js/buscar-pacientes.js
@@ -4,30 +4,15 @@ botaoAdicionar.addEventListener('click', function () {
 
   let xhr = new XMLHttpRequest(); // objeto de requisições em JavaScript
 
-  xhr.open("GET", "https://api-pacientes.herokuapp.com/pacie1ntes");
+  xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
   xhr.addEventListener('load', function(){
-<<<<<<< HEAD
     let erroAjax = document.querySelector("#erro-ajax");
+
     if ( this.status == 200 ){
       let pacientes = JSON.parse(this.responseText);
-
-      erroAjax.classList.add('invisivel');
-
-=======
-    if ( this.status == 200 ){
-      let pacientes = JSON.parse(this.responseText);
-
->>>>>>> 81ee98cf88863e4f8c15eaa9731b0e5dc112c347
       pacientes.forEach(insereNovoPaciente);
-    }else{
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-<<<<<<< HEAD
-
-=======
-      let erroAjax = document.querySelector("#erro-ajax");
->>>>>>> 81ee98cf88863e4f8c15eaa9731b0e5dc112c347
+    } else {
       erroAjax.classList.remove('invisivel');
     }
   });
