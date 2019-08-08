730b6fa8e47b9061187fc409154cf93de4b30c82
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-08 15:33:09 -0300

    início do processamento de dados com JavaScript

--- JavaScriptAvancado/aula_1/aluraframe/client/js/index.js
+++ JavaScriptAvancado/aula_1/aluraframe/client/js/index.js
@@ -1,12 +1,33 @@
-var campos = [
+let campos = [
   document.querySelector('#data'),
   document.querySelector('#quantidade'),
-  document.querySelector('valor')
+  document.querySelector('#valor')
 ];
 
 console.log(campos);
 
+let tbody = document.querySelector('table tbody');
+
 document.querySelector('.form')
         .addEventListener('submit', function(event){
-          alert('oi');
+
+          event.preventDefault();
+
+          let tr = document.createElement('tr');
+
+          campos.forEach( function (campo) {
+
+            let td = document.createElement('td');
+            td.textContent = campo.value;
+            tr.appendChild(td);
+
+          });
+          let tdVolume = document.createElement('td');
+
+          tdVolume.textContent = campos[1].value * campos[2].value;
+
+          tr.appendChild(tdVolume);
+
+          tbody.appendChild(tr);
+
         });
