850b1b80127acebac3a1454aa37499475b392b06
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-09 15:14:01 -0300

    uso de técnicas de programação defensiva

--- JavaScriptAvancado/aula_1/aluraframe/client/js/app/models/Negociacao.js
+++ JavaScriptAvancado/aula_1/aluraframe/client/js/app/models/Negociacao.js
@@ -1,7 +1,7 @@
 class Negociacao{
 
   constructor(data, quantidade, valor){
-    this._data       = data;
+    this._data       = new Date(data.getTime());
     this._quantidade = quantidade;
     this._valor      = valor;
     Object.freeze(this);
@@ -12,7 +12,7 @@ class Negociacao{
   }
 
   get data(){
-    return this._data;
+    return new Date(this._data.getTime());
   }
 
   get quantidade(){
@@ -21,5 +21,9 @@ class Negociacao{
 
   get valor(){
     return this._valor;
+  }
+
+  clone(){
+    return Object.assign(this);
   }
 }
