0e246bb4efdd441934dec56905372842363139ac
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-09 16:39:21 -0300

    criado objeto 'Negociacao' com os dados do formulário

--- JavaScriptAvancado/aula_1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScriptAvancado/aula_1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -20,9 +20,25 @@ class NegociacaoController{
   adiciona(event){
     event.preventDefault();
 
-    console.log(this._inputData.value);
-    console.log(this._inputQuantidade.value);
-    console.log(this._inputValor.value);
+    console.log(typeof(this._inputData.value));
+
+    let data = new Date(...
+                         this._inputData.value
+                             .split('-')
+                             .map(function(item, indice){
+                               return item - (indice % 2);
+                             })
+                       );
+
+    let negociacao = new Negociacao(
+      data,
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
+
+    console.log(negociacao);
+
+    // TODO: adicionar negociação na lista
 
     return this;
   }
