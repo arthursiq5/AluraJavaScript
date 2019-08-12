e7f704876b1534768eebaec24049a43e4da2a965
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-12 15:49:27 -0300

    classe documentada

--- JavaScriptAvancado/aula_1/aluraframe/client/js/app/factories/NegociacaoFactory.js
+++ JavaScriptAvancado/aula_1/aluraframe/client/js/app/factories/NegociacaoFactory.js
@@ -1,10 +1,20 @@
+/**
+ * @namespace app/Models/NegociacaoFactory
+ */
 class NegociacaoFactory{
+
+  /**
+   * @constructs
+   */
   constructor(){
     this.data = "";
     this.quantidade = 1;
     this.valor = 0.0;
   }
 
+  /**
+   * @return Negociacao negociacao
+   */
   get negociacao(){
     return new Negociacao(
                  this.data,
