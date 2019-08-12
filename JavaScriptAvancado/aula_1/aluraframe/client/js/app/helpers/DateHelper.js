fdc7ac15a5f39974472e73fb4aa88ed7c9e755dc
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-12 14:27:41 -0300

    uso de expressões regulares para testes

--- JavaScriptAvancado/aula_1/aluraframe/client/js/app/helpers/DateHelper.js
+++ JavaScriptAvancado/aula_1/aluraframe/client/js/app/helpers/DateHelper.js
@@ -16,6 +16,8 @@ class DateHelper{
    * @return Date data
    */
   static textoParaData(texto){
+    if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+      throw new Error(`Deve estar no formato aaaa-mm-dd`);
     return new Date(...(texto.split('-').map((item, indice) => item - indice % 2)));
   }
 
@@ -24,8 +26,6 @@ class DateHelper{
    * @return string data
    */
   static dataParaTexto(data){
-    return   data.getDate()
-    + '/' + (data.getMonth() + 1)
-    + '/' + data.getFullYear();
+    return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
   }
 }
