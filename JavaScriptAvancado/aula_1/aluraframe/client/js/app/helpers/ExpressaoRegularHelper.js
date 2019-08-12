326109be6c38510b16c46a2fe7eaad7a8fbc066c
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-12 15:55:11 -0300

    aprimorada expressão regular para resultados mais precisos

--- JavaScriptAvancado/aula_1/aluraframe/client/js/app/helpers/ExpressaoRegularHelper.js
+++ JavaScriptAvancado/aula_1/aluraframe/client/js/app/helpers/ExpressaoRegularHelper.js
@@ -17,7 +17,7 @@ class ExpressaoRegularHelper{
    * @return boolean resultado
    */
   static validaData(data){
-    let expressao = /\d{4}-\d{2}-\d{2}/;
+    let expressao = /^\d{4}-\d{2}-\d{2}$/;
     return expressao.test(data);
   }
 }
