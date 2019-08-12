dc819eb8f83f472e5b6f12bdd95f2f848e408f0b
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-12 15:47:45 -0300

    criada classe helper para validação de expressões regulares

--- JavaScriptAvancado/aula_1/aluraframe/client/js/app/helpers/DateHelper.js
+++ JavaScriptAvancado/aula_1/aluraframe/client/js/app/helpers/DateHelper.js
@@ -16,7 +16,7 @@ class DateHelper{
    * @return Date data
    */
   static textoParaData(texto){
-    if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+    if(!ExpressaoRegularHelper.validaData(texto))
       throw new Error(`Deve estar no formato aaaa-mm-dd`);
     return new Date(...(texto.split('-').map((item, indice) => item - indice % 2)));
   }
---
+++ JavaScriptAvancado/aula_1/aluraframe/client/js/app/helpers/ExpressaoRegularHelper.js
@@ -0,0 +1,23 @@
+/**
+ * @description validador de expressões regulares
+ * @namespace app/helpers/DateHelper
+ */
+class ExpressaoRegularHelper{
+
+  /**
+   * @throws Error ExpressaoRegular não pode ser instanciado
+   * @constructs
+   */
+  constructor(){
+    throw new Error("ExpressaoRegular não pode ser instanciada");
+  }
+
+  /**
+   * @param string data
+   * @return boolean resultado
+   */
+  static validaData(data){
+    let expressao = /\d{4}-\d{2}-\d{2}/;
+    return expressao.test(data);
+  }
+}
