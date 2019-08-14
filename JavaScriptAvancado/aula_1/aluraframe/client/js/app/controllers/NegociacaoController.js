40ca25e4a0713a364c141c88f2fe03d4c29db5f0
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-14 15:39:15 -0300

    usando associação de dados para atualização de views

--- JavaScriptAvancado/aula_1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScriptAvancado/aula_1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -15,18 +15,22 @@ class NegociacaoController{
     this._inputValor      = $('#valor');
 
     this._negociacoesView  = new NegociacoesView($("#negociacoesView"));
-    this._listaNegociacoes = ProxyFactory.create(
-                              new ListaNegociacoes(),
-                              ['adiciona', 'esvazia'],
-                              (model) => this._negociacoesView.update(model)
-                            );
-    this._mensagemView     = new MensagemView($("#mensagemView"));
-    this._mensagem         = ProxyFactory.create(
-                               new Mensagem(),
-                               ['texto'],
-                               (model) => this._mensagemView.update(model)
+    this._listaNegociacoes = new Bind(
+                               new ListaNegociacoes(),
+                               this._negociacoesView,
+                               ['adiciona', 'esvazia']
                              );
-    this._negociacoesView.update(this._mensagem);
+    // ProxyFactory.create(
+    //                           new ListaNegociacoes(),
+    //                           ['adiciona', 'esvazia'],
+    //                           (model) => this._negociacoesView.update(model)
+    //                         );
+    this._mensagemView     = new MensagemView($("#mensagemView"));
+    this._mensagem         = new Bind(
+                               new Mensagem(),
+                               this._mensagemView,
+                               ['texto']
+                             );
   }
 
   /**
