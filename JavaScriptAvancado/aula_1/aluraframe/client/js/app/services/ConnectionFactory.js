0ee40b02cf9ef472fb626053ddc2d457f63e5f5a
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-16 16:07:43 -0300

    Criação de classe estática efetuada

--- JavaScriptAvancado/aula_1/aluraframe/client/js/app/services/ConnectionFactory.js
+++ JavaScriptAvancado/aula_1/aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,42 +1,45 @@
 var stores = ['negociacoes'];
-var version = 1;
-var dbName = 'aluraframe';
-var connection = null;
 
-class ConnectionFactory{
+var ConnectionFactory = (function (){
+  var version = 1;
+  var dbName = 'aluraframe';
+  var connection = null;
 
-  constructor(){
-    throw new Error('Não é possível criar instâncias de ConnectionFactory');
+  return class ConnectionFactory{
+
+    constructor(){
+      throw new Error('Não é possível criar instâncias de ConnectionFactory');
+    }
+
+    static getConnection(){
+      return new Promise( (resolve, reject) => {
+        let openRequest = window.indexedDB.open(dbName, version);
+
+        openRequest.onupgradeneeded = evento =>
+          ConnectionFactory._createStores(evento.target.result);
+
+        openRequest.onsuccess = evento => {
+          if(!connection)
+            connection = evento.target.result
+          resolve(connection);
+        };
+
+        openRequest.onerror = evento => {
+          console.log(evento.target.error);
+
+          reject(evento.target.error.name);
+        };
+      });
+    }
+
+    static _createStores(connection){
+      stores.forEach(store => {
+
+        if(connection.objectStoreNames.contains(store))
+          connection.deleteObjectStore(store);
+
+        connection.createObjectStore(store, { autoIncrement:true });
+      });
+    }
   }
-
-  static getConnection(){
-    return new Promise( (resolve, reject) => {
-      let openRequest = window.indexedDB.open(dbName, version);
-
-      openRequest.onupgradeneeded = evento =>
-        ConnectionFactory._createStores(evento.target.result);
-
-      openRequest.onsuccess = evento => {
-        if(!connection)
-          connection = evento.target.result
-        resolve(connection);
-      };
-
-      openRequest.onerror = evento => {
-        console.log(evento.target.error);
-
-        reject(evento.target.error.name);
-      };
-    });
-  }
-
-  static _createStores(connection){
-    stores.forEach(store => {
-
-      if(connection.objectStoreNames.contains(store))
-        connection.deleteObjectStore(store);
-
-      connection.createObjectStore(store, { autoIncrement:true });
-    });
-  }
-}
+})();
