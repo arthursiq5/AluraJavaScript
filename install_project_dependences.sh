22c3f247d12f701268ce896cdfe98dec2aef8a11
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-21 14:47:07 -0300

    aprimorada automatização de instalação de módulos

--- install_project_dependences.sh
+++ install_project_dependences.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
+LOCAL=$(pwd)
+JAVASCRIPT_AVANCADO_PATH="$LOCAL/JavaScriptAvancado/aula_1/aluraframe/"
+CLIENT_PATH="$JAVASCRIPT_AVANCADO_PATH/client"
+SERVER_PATH="$JAVASCRIPT_AVANCADO_PATH/server"
+
 git submodule init
 git submodule update
+cd $JAVASCRIPT_AVANCADO_PATH
+
+cd $CLIENT_PATH
+pwd
+bash install_npm_client_dependences.sh
+
+cd $SERVER_PATH
+pwd
+bash install_npm_server_dependences.sh
